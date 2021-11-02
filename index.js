c930f234bafcf6086c04da51435d7cba5baa0cfb
DanielPhilosoph <danielph9@gmail.com>
2021-11-02 14:30:30 +0200

    Basic Stracture
    created html + css + functions for events

---
+++ index.js
@@ -0,0 +1,11 @@
+// ==============================
+// ====== Add Events ============
+// ==============================
+document.querySelector("#activate").addEventListener("click", onActivateClick);
+document.querySelector("#copyToClip").addEventListener("click", onCopyClick);
+
+// ==============================
+// ====== Event Listeners =======
+// ==============================
+function onActivateClick() {}
+function onCopyClick() {}
