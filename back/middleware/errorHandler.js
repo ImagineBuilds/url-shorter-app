4474936190a5689ee88fce4d1dab3f9e8a70dce3
DanielPhilosoph <danielph9@gmail.com>
2021-11-03 10:13:18 +0200

    error handling

--- back/middleware/errorHandler.js
+++ back/middleware/errorHandler.js
@@ -1,5 +1,11 @@
 function errorHandler(err, req, res, next) {
-  res.status(500).json({ message: err });
+  if (err.error === 403) {
+    res.status(err.error).json({ message: err.message });
+  } else if (err.error === 402) {
+    res.status(err.error).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: err });
+  }
 }
 
 module.exports = errorHandler;
