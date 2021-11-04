8b620fcc8f09ecbc79cce5eee891387a9848e855
DanielPhilosoph <danielph9@gmail.com>
2021-11-04 10:46:12 +0200

    Finished statistics
    Count logs - and saves the date, showing on board

--- front/index.js
+++ front/index.js
@@ -2,9 +2,11 @@
 // ==== Server requests =========
 // ==============================
 
-async function getInfo() {
+async function getInfo(shortUrl) {
   try {
-    let response = await axios.get("http://localhost:3000/api/shorturl/info/1");
+    let response = await axios.get(
+      `http://localhost:3000/api/shorturl/info/${shortUrl}`
+    );
     return response.data;
   } catch (error) {
     console.log(error);
@@ -46,6 +48,12 @@ document.querySelector("#copyToClip").addEventListener("click", onCopyClick);
 document
   .querySelector("#activateCustom")
   .addEventListener("click", onActivateCustomClick);
+document
+  .querySelector("#getStatisticsMonth")
+  .addEventListener("click", onStatisticsMonthClick);
+document
+  .querySelector("#getStatisticsYear")
+  .addEventListener("click", onStatisticsYearClick);
 
 // ==============================
 // ====== Event Listeners =======
@@ -72,9 +80,92 @@ async function onActivateCustomClick() {
     "http://localhost:3000/api/shorturl/" + short;
 }
 
-async function f() {
-  let obj = await getInfo();
-  console.log(obj);
+async function onStatisticsMonthClick() {
+  let obj = await getInfo(document.querySelector("#satisticInput").value);
+  let date = new Date(document.querySelector("#satisticInputMonth").value);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let currentYearArray = obj.redirectEntriesLog.map((date) => {
+    if (new Date(date).getFullYear() === year) return new Date(date);
+  });
+  createChart(
+    `Entries for year ${year} in ${date.toLocaleString("en-US", {
+      month: "long",
+    })}`,
+    getColumnsDays,
+    currentYearArray,
+    year,
+    month
+  );
 }
 
-f();
+async function onStatisticsYearClick() {
+  let obj = await getInfo(document.querySelector("#satisticInput").value);
+  let date = new Date(document.querySelector("#satisticInputMonth").value);
+  let year = date.getFullYear();
+  let currentYearArray = obj.redirectEntriesLog.map((date) => {
+    if (new Date(date).getFullYear() === year) return new Date(date);
+  });
+  createChart(
+    `Entries for year ${year}`,
+    getColumnsMonth,
+    currentYearArray,
+    year
+  );
+}
+
+function createChart(
+  header,
+  columsFunction,
+  logsArray,
+  year,
+  month = undefined
+) {
+  let chart = new CanvasJS.Chart("chartContainer", {
+    title: {
+      text: header,
+    },
+    axisY: {
+      title: "Entries",
+    },
+    axisX: {
+      title: "Month",
+    },
+    data: [
+      {
+        type: "line",
+        dataPoints: columsFunction(logsArray, year, month),
+      },
+    ],
+  });
+  chart.render();
+}
+
+function countMonthEntries(array, month) {
+  return array.filter((date) => {
+    return date.getMonth() + 1 === month;
+  });
+}
+
+function countDayEntries(array, month, day) {
+  return array.filter((date) => {
+    return date.getMonth() + 1 === month && date.getDate() === day;
+  });
+}
+
+function getColumnsMonth(logArray) {
+  let array = [];
+  for (let month = 1; month <= 12; month++) {
+    array.push({ label: month, y: countMonthEntries(logArray, month).length });
+  }
+  return array;
+}
+
+function getColumnsDays(logArray, year, month) {
+  let array = [];
+  let numberOfDays = new Date(year, month, 0).getDate();
+  for (let day = 1; day <= numberOfDays; day++) {
+    array.push({ label: day, y: countDayEntries(logArray, month, day).length });
+  }
+  return array;
+}
